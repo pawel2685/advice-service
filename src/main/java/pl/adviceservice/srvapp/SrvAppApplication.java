e73dbb338ff83ac92ba6636f2f6e18edd190374f
Pawel <pawmag.code@gmail.com>
2019-11-21 17:38:50 +0100

    #Setup configuration + welcome page

--- src/main/java/pl/adviceservice/srvapp/SrvAppApplication.java
+++ src/main/java/pl/adviceservice/srvapp/SrvAppApplication.java
@@ -2,12 +2,20 @@ package pl.adviceservice.srvapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
-public class SrvAppApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(SrvAppApplication.class, args);
+// Using a root package also allows the @ComponentScan annotation to be used without needing to specify a basePackage attribute
+public class SrvAppApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SrvAppApplication.class);
     }
 
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SrvAppApplication.class, args);            // it will start application
+    }
 }
