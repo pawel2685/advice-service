43550decd11b9745c066abdca87dff1ebb8c63b3
Pawel <pawmag.code@gmail.com>
2019-11-21 13:12:05 +0100

    #Initial commit

---
+++ src/main/java/pl/adviceservice/srvapp/SrvAppApplication.java
@@ -0,0 +1,13 @@
+package pl.adviceservice.srvapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrvAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrvAppApplication.class, args);
+    }
+
+}
